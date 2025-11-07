fd271966138007604fe892e075761f4c5ca52396
Hudayyar Yusubov <mr.allanurov@inbox.ru>
2025-11-07 13:42:44 +0800

    Fix SupabaseController mapping

--- src/main/java/com/momentz/controller/SupabaseController.java
+++ src/main/java/com/momentz/controller/SupabaseController.java
@@ -2,9 +2,11 @@ package com.momentz.controller;
 
 import com.momentz.service.SupabaseService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/supabase")
 public class SupabaseController {
 
     private final SupabaseService supabaseService;
@@ -13,7 +15,7 @@ public class SupabaseController {
         this.supabaseService = supabaseService;
     }
 
-    @GetMapping("/supabase/posts")
+    @GetMapping("/posts")
     public String getPosts() throws Exception {
         return supabaseService.getPosts();
     }
